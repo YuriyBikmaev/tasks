2220d1f9a59288b2b4dcbec7b55bd01865d697ec
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-19 10:48:54 +0400

    Написана программа поиска максимального из трех чисел

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberС = int.Parse(Console.ReadLine());
+if ((numberA != numberB) || (numberA != numberС))
+{
+    if ((numberA > numberB) && (numberA > numberС)) 
+    {
+        Console.WriteLine($"Максимальное число = {numberA}");
+    }
+    else if (numberB > numberС)
+    {
+        Console.WriteLine($"Максимальное число = {numberB}");
+    }
+    else 
+    {
+        Console.WriteLine($"Максимальное число = {numberС}");
+    }
+} else Console.WriteLine("Числа равны");
