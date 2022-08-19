cfeefb7d004a6913973712061788c9955184bb02
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-19 10:18:09 +0400

    Написана программа поиска максимального из двух введенных чисел

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA != numberB)
+{
+    if (numberA > numberB) 
+    {
+        Console.WriteLine($"Максимальное число = {numberA}");
+    }
+    else 
+    {
+        Console.WriteLine($"Максимальное число = {numberB}");
+    }
+} else Console.WriteLine("Числа равны");
