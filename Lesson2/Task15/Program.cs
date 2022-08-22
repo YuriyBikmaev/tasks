20fad2403d4aa5b74dbc0ac27deca4ff8694b418
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-22 23:46:05 +0400

    Добавлена программа, которая проверяет введенное число является ли выходным днем недели

---
+++ Lesson2/Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 0 && number < 8)
+{
+    if (number>5)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} дня недели не существует");
+}
