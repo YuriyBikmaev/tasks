59a27f2c5978e36192170f7ea021a7033c8157dc
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-22 23:15:28 +0400

    Добавлена программа вывода второго числа в трехзначном

---
+++ Lesson2/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа на вход принимает трехзначное число, а на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine($"{number} -> {(number%100)/10}");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является трехначным");
+}
