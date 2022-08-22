0fbf91e4715f21f3a2a1ba226c2ab4bdfd8e3120
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-22 23:31:01 +0400

    Добавлена программа, которая выводит третью цифру с конца или сообщает что третьей цифры нет

---
+++ Lesson2/Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа выводит третью цифру числа с конца или сообщает что третьей цифры нет
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>99)
+{
+    Console.WriteLine($"{number} -> {(number%1000)/100}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
