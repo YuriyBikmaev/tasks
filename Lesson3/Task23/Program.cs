6ec18d1b073bd9ddcbf1b042d5beecba124e4b28
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-25 23:14:12 +0400

    Написана программа, которая выводит таблицу кубов от 1 до N

---
+++ Lesson3/Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа на вход принимает число (N) и выдает таблицу кубов от 1 до N
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+for (int i=1; i<=numberN; i++)
+{
+     Console.Write($"{i*i*i}, ");
+}
+Console.WriteLine();
