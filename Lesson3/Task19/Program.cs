5eac1df2e70061be4dfcf1501ff9a8eed87ae7da
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-08-25 23:12:41 +0400

    Написана программа проверки числа на палиндром

---
+++ Lesson3/Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа на вход принимает пятизначное число и проверяет, является ли оно палиндромом
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>9999 && number<100000)
+{
+    int numberLast2 = number%100;
+    if ((number/1000) == ((numberLast2%10)*10+numberLast2/10))
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+else 
+{
+    Console.WriteLine($"Число {number} не является пятизначным");
+}
