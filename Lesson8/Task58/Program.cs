d5b3d053837f310a5e35bb95fbc618b75a8a1a1e
Yuriy Bikmaev <y.bikmaev@yandex.ru>
2022-09-14 19:24:17 +0400

    Внесены исправления в задание 58

--- Lesson8/Task58/Program.cs
+++ Lesson8/Task58/Program.cs
@@ -1,8 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Программа находит произведение матриц
 
-int[,] InitArray(int size, int minValue, int maxValue)
+int[,] InitArray(int row, int col, int minValue, int maxValue)
 {
-    int[,] arr = new int[size, size];
+    int[,] arr = new int[row, col];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
@@ -25,35 +25,36 @@ void PrintArray(int[,] arr)
     }
 }
 
-int MatrixMultiplicationStep (int curPosition, int[,] array, int col)
-{
-    int size = array.GetLength(0);
-    int result = 0;
-    for (int i = 0; i < size; i++)
-    {
-        result+= curPosition* array[i,col];
-    }
-    return result;
-}
 
 int[,] MatrixMultiplication(int[,] oneArr, int[,] twoArr)
 {
-    int size = oneArr.GetLength(0);
-    int[,] result = new int[size, size];
-    for (int i = 0; i < size; i++)
+    int rowM1 = oneArr.GetLength(0);
+    int colM1 = oneArr.GetLength(1);
+    int colM2 = twoArr.GetLength(1);
+    int[,] result = new int[rowM1, colM2];
+    for (int i = 0; i < rowM1; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (int j = 0; j < colM2; j++)
         {
-            result[i,j] = MatrixMultiplicationStep(oneArr[i,j], twoArr, j);
+            for (int k = 0; k < colM1; k++)
+            {
+                   result[i, j] += oneArr[i, k] * twoArr[k, j];
+            }
         }
     }
     return result;
 }
 
-Console.Write("Введите размер матрицы: ");
-int sizeMatrix = int.Parse(Console.ReadLine());
-int[,] oneMatrix = InitArray(sizeMatrix, 1, 9);
-int[,] twoMatrix = InitArray(sizeMatrix, 1, 9);
+Console.Write("Введите количество строк первой матрицы: ");
+int rowMatrix1 = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов первой матрицы: ");
+int colMatrix1 = int.Parse(Console.ReadLine());
+Console.Write("Введите количество строк второй матрицы: ");
+int rowMatrix2 = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов второй матрицы: ");
+int colMatrix2 = int.Parse(Console.ReadLine());
+int[,] oneMatrix = InitArray(rowMatrix1, colMatrix1, 0, 9);
+int[,] twoMatrix = InitArray(rowMatrix2, colMatrix2, 0, 9);
 
 Console.WriteLine();
 Console.WriteLine("Первая матрица:");
@@ -63,7 +64,14 @@ Console.WriteLine();
 Console.WriteLine("Вторая матрица:");
 PrintArray(twoMatrix);
 
-int[,] resultMatrix = InitArray(sizeMatrix, 1, 9);
-Console.WriteLine();
-Console.WriteLine("Произведение матриц:");
-PrintArray(twoMatrix);
+if (colMatrix1 == rowMatrix2)
+{
+    int[,] resultMatrix = MatrixMultiplication(oneMatrix, twoMatrix);
+    Console.WriteLine();
+    Console.WriteLine("Произведение матриц:");
+    PrintArray(resultMatrix);
+}
+else
+    Console.WriteLine(
+        "Количество столбцов первой матрицы должно быть ровно количеству строк второй матрицы!"
+    );
